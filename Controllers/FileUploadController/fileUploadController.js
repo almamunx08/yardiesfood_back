9d1eec2ab932dc55f0a0e0f74a876835d129d2e1
masudrana08 <rana028511@gmail.com>
2022-10-08 16:16:33 +0600

    file upload with error handle

--- Controllers/FileUploadController/fileUploadController.js
+++ Controllers/FileUploadController/fileUploadController.js
@@ -1,6 +1,35 @@
-const fileUploadController = (req, res) => {
-   
-   res.send({status: 'Uploaded Successfully', url: req.file.filename})
-}
+const path = require("path");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/img");
+  },
+  filename: function (req, file, cb) {
+    if (file.mimetype == ("image/png" || "image/jpg" || "image/jpeg")) {
+      const fileName = path.parse(file.originalname);
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      cb(
+        null,
+        fileName.name.toLowerCase().split(" ").join("-") +
+          "-" +
+          uniqueSuffix +
+          fileName.ext
+      );
+    } else {
+      cb(new Error("Bad file"));
+    }
+  },
+});
 
-module.exports = fileUploadController
+const fileUploadController = (req, res) => {
+  const upload = multer({ storage: storage }).single("img");
+  upload(req, res, (error) => {
+    if (error) {
+      res.send("something wrong ");
+    } else {
+      res.send({ status: "Uploaded Successfully", url: req.file.filename });
+    }
+  });
+};
+
+module.exports = fileUploadController;
--- app.js
+++ app.js
@@ -8,20 +8,6 @@ const UserRoute = require('./Routes/UserRoute/UserRoute');
 const whoIs = require('./Middleware/whois');
 const fileUploadController = require('./Controllers/FileUploadController/FileUploadController');
 const PORT = process.env.PORT || 8000
-const multer  = require('multer')
-const path = require('path')
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/img/')
-    },
-    filename: function (req, file, cb) {
-        const fileName = path.parse(file.originalname)
-      const uniqueSuffix = Date.now() +"-"+ Math.round(Math.random() * 1E9)
-      cb(null, fileName.name.toLowerCase().split(' ').join('-') + '-' + uniqueSuffix + fileName.ext)
-    }
-  })
-const upload = multer({ storage: storage })
 
 app.use(express.static('./public'))
 
@@ -30,7 +16,7 @@ app.use(express.json())
 
 app.use('/api/auth', AuthRoute)
 app.use('/api/user', whoIs, UserRoute)
-app.use('/api/fileUpload', upload.single('img'), fileUploadController)
+app.use('/api/fileUpload', fileUploadController)
 
 app.listen(PORT, function () {
     console.log("Listening on port "+PORT);
